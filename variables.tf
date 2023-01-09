a3a2ac1e4ca6283ee0d5f8a80d6a8c3d573e471c
Nirajsingh95 <94783723+Nirajsingh95@users.noreply.github.com>
2023-01-09 22:49:32 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "ap-south-1"
+}
+
+variable "key_name" {
+  description = " SSH keys to connect to ec2 instance"
+  default     =  "test"
+}
+
+variable "instance_type" {
+  description = "instance type for ec2"
+  default     =  "t2.micro"
+}
+
+variable "security_group" {
+  description = "Name of security group"
+  default     = "default"
+}
+variable "aws_iam_role" {
+  description = "Name of the IAM role"
+  default     = "ec2-tf"
+}
+variable "tag_name" {
+  description = "Tag Name of for Ec2 instance"
+  default     = "Test_instance"
+}
+
+variable "ami_id" {
+  description = "AMI for Ec2 instance"
+  default     = "ami-0cca134ec43cf708f"
+}
